8ce223bad67776df1cfc389d78875948d925be47
Chirag Verma <chirag.verma@FFPTN2202.local>
2020-04-30 00:37:31 +0100

    DockerFile

--- buildShopfloorApp.sh
+++ buildShopfloorApp.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-xcrun xcodebuild -workspace ConsumerApp.xcworkspace -scheme "Tage-In-House-Enterprise" -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 11,OS=13.2'
-ios-sim start --devicetypeid com.apple.CoreSimulator.SimDeviceType.iPhone-11
-xcrun simctl install 2DD00C38-8F4A-41B5-B37D-C9E3D8886FF3 /Users/chirag.verma/Library/Developer/Xcode/DerivedData/ConsumerApp-chkgycoenlldgdeegksnhvtcyohh/Build/Products/In-House-Enterprise-iphonesimulator/Tage.app
+xcrun xcodebuild -workspace Shopfloor.xcworkspace -scheme "In-House" -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 8,OS=13.2'
+ios-sim start --devicetypeid com.apple.CoreSimulator.SimDeviceType.iPhone-7
+ios-sim start --devicetypeid com.apple.CoreSimulator.SimDeviceType.iPhone-8
+xcrun simctl install 2DE2ADEA-6B37-40A4-8E69-FCCC30F5E483 /Users/chirag.verma/Library/Developer/Xcode/DerivedData/ConsumerApp-chkgycoenlldgdeegksnhvtcyohh/Build/Products/In-House-Enterprise-iphonesimulator/Tage.app
+xcrun simctl install 2EAA4629-7260-42DB-84DA-AD2E1F662C61 /Users/chirag.verma/Library/Developer/Xcode/DerivedData/ConsumerApp-chkgycoenlldgdeegksnhvtcyohh/Build/Products/In-House-Enterprise-iphonesimulator/Tage.app
