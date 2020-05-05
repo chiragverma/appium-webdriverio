bf5288963f3639701bd363feddbeefa1034db024
Chirag Verma <chirag.verma@farfetch.com>
2020-05-05 17:26:44 +0000

    Update buildConsumerApp.sh

--- buildConsumerApp.sh
+++ buildConsumerApp.sh
@@ -4,8 +4,8 @@ bundle update
 bundle exec pod repo update
 bundle exec pod install
 # builds the code
-xcrun xcodebuild -workspace ConsumerApp.xcworkspace -scheme "Tage-In-House-Enterprise" -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 11,OS=13.4.1'
-# opens the simulator
-ios-sim start --devicetypeid com.apple.CoreSimulator.SimDeviceType.iPhone-11
-# downloads the app in the simulator
-xcrun simctl install BE45E1CD-BC06-42E8-9EF3-B2FCDEA968CF /Users/chirag.verma/Library/Developer/Xcode/DerivedData/ConsumerApp-chkgycoenlldgdeegksnhvtcyohh/Build/Products/In-House-Enterprise-iphonesimulator/Tage.app
+xcrun xcodebuild -workspace ConsumerApp.xcworkspace -scheme "Tage-In-House-Enterprise" -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 8,OS=13.4.1'
+# starts the iphone simulator
+ios-sim start --devicetypeid com.apple.CoreSimulator.SimDeviceType.iPhone-8
+# downloads the app to the simulator
+xcrun simctl install 0A21DF3F-F8D3-4905-983D-EECC99CF89F2 /Users/chirag.verma/Library/Developer/Xcode/DerivedData/Shopfloor-dlpwdmtxpnpgdxgvpkyneiomysay/Build/Products/In-House-iphonesimulator/Shopfloor.app
