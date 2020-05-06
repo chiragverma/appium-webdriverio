9acea163665d0d7cfca3ba1d3ccc780a53cc8dba
Chirag Verma <chirag.verma@farfetch.com>
2020-05-06 23:33:15 +0000

    Update buildConsumerApp.sh

--- buildConsumerApp.sh
+++ buildConsumerApp.sh
@@ -4,8 +4,8 @@ bundle update
 bundle exec pod install
 bundle exec pod repo update
 # builds the code
-xcrun xcodebuild -workspace ConsumerApp.xcworkspace -scheme "Tage-In-House-Enterprise" -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 8,OS=13.4.1'
+xcrun xcodebuild -workspace ConsumerApp.xcworkspace -scheme "Tage-In-House-Enterprise" -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 8,OS=13.4.1' || { echo 'ConsumerApp Build Failed' ; exit 1; }
 # starts the iphone simulator
 ios-sim start --devicetypeid com.apple.CoreSimulator.SimDeviceType.iPhone-8
 # downloads the app to the simulator
-xcrun simctl install 0A21DF3F-F8D3-4905-983D-EECC99CF89F2 /Users/chirag.verma/Library/Developer/Xcode/DerivedData/ConsumerApp-dpyhndtiqafzkifahqcdpmlscxxc/Build/Products/In-House-Enterprise-iphonesimulator/Tage.app
+xcrun simctl install 0A21DF3F-F8D3-4905-983D-EECC99CF89F2 /Users/chirag.verma/Library/Developer/Xcode/DerivedData/ConsumerApp-dpyhndtiqafzkifahqcdpmlscxxc/Build/Products/In-House-Enterprise-iphonesimulator/Tage.app || { echo 'Unable to install Consumer App' ; exit 1; }
