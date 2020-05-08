307fb91115fd4106ec8cf759c4eb4fc310849a1b
Chirag Verma <chirag.verma@farfetch.com>
2020-05-08 12:33:33 +0000

    Update buildMirror.sh

--- buildMirror.sh
+++ buildMirror.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 export PATH=/usr/local/bin:$PATH
-cd /Users/chirag.verma/mirror && yarn
-cd /Users/chirag.verma/mirror/packages/tage && yarn dev &
-# start another shell and replacing the current
-exec /bin/bash
+cd $HOME/mirror && yarn
+cd $HOME/mirror/packages/tage && yarn dev &
