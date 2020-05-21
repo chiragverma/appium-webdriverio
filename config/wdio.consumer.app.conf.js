1a49b04c155e8b1207f9002ee69337c09ba3f5d2
Chirag Verma <chirag.verma@farfetch.com>
2020-05-21 22:56:14 +0000

    Update wdio.consumer.app.conf.js

--- config/wdio.consumer.app.conf.js
+++ config/wdio.consumer.app.conf.js
@@ -47,7 +47,7 @@ exports.config = {
     //bail: 0,
     //
     reporters: [['allure', {
-        outputDir: 'allureresults',
+        outputDir: 'allure-results',
         disableWebdriverStepsReporting: true,
         disableWebdriverScreenshotsReporting: true,
     }]],
