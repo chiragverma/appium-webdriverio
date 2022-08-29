869c5f8d4054af08b0dde4b400af3dd82e17b515
Chirag Verma <chirag.verma@hotmail.com>
2022-08-30 04:16:06 +0800

    Update HomeScreen.ts

--- tests/screenobjects/HomeScreen.ts
+++ tests/screenobjects/HomeScreen.ts
@@ -1,7 +1,7 @@
 class HomeScreen {
 
-    get instructionsTab () {return $(browser.isAndroid ? '//android.view.View[contains(@resource-id, "Settings tab")]' : '~Settings tab')}
-    get dashboardTab () {return $(browser.isAndroid ? '//android.view.View[contains(@resource-id, "Settings tab")]' : '~Settings tab')}
+    get instructionsTab () {return $(browser.isAndroid ? '//android.view.View[contains(@resource-id, "Instructions tab")]' : '~Instructions tab')}
+    get dashboardTab () {return $(browser.isAndroid ? '//android.view.View[contains(@resource-id, "Dashboard tab")]' : '~Dashboard tab')}
     get settingsTab () {return $(browser.isAndroid ? '//android.view.View[contains(@resource-id, "Settings tab")]' : '~Settings tab')}
 
 
